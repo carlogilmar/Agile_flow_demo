00faadaf772f5aa9d3e904961b4cc3467aa58021
carlogilmar <carlogilmar12@gmail.com>
2018-10-24 01:52:14 -0500

    #11 Updating counters when login and logout

--- lib/agile_flow_web/channels/main_socket.ex
+++ lib/agile_flow_web/channels/main_socket.ex
@@ -13,12 +13,14 @@ defmodule AgileFlowWeb.MainChannel do
     {:ok, _} = Presence.track(socket, user["user_id"], user)
     presence_list = Presence.list(socket)
     push socket, "presence_state", presence_list
+    counters = count_users( socket )
+    push socket, "main::team_players", counters
     {:noreply, socket}
   end
 
   def handle_in("main::sync_users", _payload, socket) do
     counters = count_users( socket )
-    IO.inspect counters
+    push socket, "main::team_players", counters
     {:noreply, socket}
   end
 
@@ -36,9 +38,20 @@ defmodule AgileFlowWeb.MainChannel do
         [meta] = tracking[user].metas
         meta["category"]
       end
-    categories |> get_counters()
+    categories |> get_counters() |> fill_empty_keys()
   end
 
   def get_counters( categories ), do: categories |> Enum.reduce( %{}, fn state, counter -> Map.put( counter, state, (counter[state] || 0) + 1 ) end )
+
+  def fill_empty_keys( counters_map ) do
+    counters = %{}
+    keys = ["A", "B", "C", "D", "E", "F", "G", "H"]
+    [a,b,c,d,e,f,g,h] = for key <- keys, do: counter = get_counter( counters_map[ key ] )
+    %{ "A" => a, "B" => b, "C" => c, "D" => d, "E" => e, "F" => f, "G" => g, "H" => h }
+  end
+
+  defp get_counter( nil ), do: 0
+  defp get_counter( n ), do: n
+
 
 end
