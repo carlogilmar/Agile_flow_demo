b719142e38bf9f0a528021ff95d61ac20dafd690
carlogilmar <carlogilmar12@gmail.com>
2018-10-23 22:52:05 -0500

    #9 learning/9 Send an animal per second

--- lib/agile_flow/session_game.ex
+++ lib/agile_flow/session_game.ex
@@ -1,6 +1,8 @@
 defmodule AgileFlow.SessionGame do
   use GenServer
 
+  @animals ["eagle", "tapir", "insect", "wolf", "tiger", "elephant"]
+
   def start_link() do
     GenServer.start_link(__MODULE__, [], [name: __MODULE__])
   end
@@ -9,17 +11,22 @@ defmodule AgileFlow.SessionGame do
   def end_game(), do: GenServer.cast( __MODULE__, {:end} )
 
   def init(_) do
-    {:ok, {:counter, 0} }
+    {:ok, {:counter, 0, :animals, 0} }
   end
 
   defp loop(), do: send self(), :loop
 
   def handle_info(:loop, state) do
-    {_, counter} = state
+    {_, counter, _, animals_index} = state
+    IO.puts "Segundo #{counter}, animal #{get_animal( animals_index )}"
+    counters = get_counters( counter, animals_index)
     :timer.sleep 1000
     loop()
-    {:noreply, {:counter, counter+1} }
+    {:noreply, counters }
   end
+  defp get_animal( index ), do: Enum.at( @animals, index )
+  defp get_counters( counter, 5), do: {:counter, counter+1, :animals, 0}
+  defp get_counters( counter, animals ), do: {:counter, counter+1, :animals, animals+1}
 
   def handle_cast( {:start}, state) do
     IO.puts "Empezando el contador"
