7f04c079cf50c796902db41f7408210e62ab27a0
carlogilmar <carlogilmar12@gmail.com>
2018-10-23 23:57:46 -0500

    #9 Choose an animal by period of time

--- lib/agile_flow/session_game.ex
+++ lib/agile_flow/session_game.ex
@@ -8,26 +8,40 @@ defmodule AgileFlow.SessionGame do
   def reset(), do: GenServer.cast( __MODULE__, {:end} )
   defp loop(), do: send self(), :loop
 
-  def init(_), do: {:ok, {:timer, 0, :animals, 0, :enable, 0} }
+  def init(_), do: {:ok, {:timer, 0, :animals, 0, :enable_flag, 0, :disable, :no_animal_selected} }
 
   def handle_info(:loop, state) do
-    {_, counter, _, animals_index, _, enable_counter} = state
-    { next_enable_counter, current_state } = get_enable_counter( counter, enable_counter, counter == enable_counter )
-    animal = get_animal( animals_index )
-    IO.puts "Segundo #{counter}, animal #{animal} Estado: #{current_state} #{next_enable_counter}"
-    # send broadcast
-    { next_timer, next_animal_index} = get_counters( counter, animals_index)
+    {_, counter, _, animals_index, _, enable_counter, current, animal} = state
+    next_state = get_next_state( counter, animals_index, enable_counter )
+    IO.inspect next_state
     :timer.sleep 1000
     loop()
-    {:noreply, {:timer, next_timer, :animals, next_animal_index, :enable, next_enable_counter} }
+    {:noreply, next_state}
   end
 
+  defp get_next_state( counter, animals_index, enable_counter ) do
+    { next_enable_counter, next_current_state } = get_enable_counter( counter, enable_counter, counter == enable_counter)
+    { next_animals_index, next_animal } = get_next_animal( animals_index, next_current_state )
+    {:timer, counter+1, :animals, next_animals_index, :enable_flag, next_enable_counter, next_current_state, next_animal}
+  end
+
+  def get_next_animal( index, :disable) do
+    next_index = get_next_animal_index( index )
+    next_animal = get_animal( next_index )
+    { next_index, next_animal }
+  end
+  def get_next_animal( index, :enable) do
+    animal = get_animal( index )
+    { index, animal }
+  end
+
+  # Define how many seconds we are going to change the animal
   defp get_enable_counter( current, _, true), do: { current + 5, :disable}
   defp get_enable_counter( _, enable_counter, false), do: { enable_counter, :enable }
 
   defp get_animal( index ), do: Enum.at( @animals, index )
-  defp get_counters( counter, 5), do: {counter+1, 0}
-  defp get_counters( counter, animals ), do: {counter+1, animals+1}
+  defp get_next_animal_index(5), do: 0
+  defp get_next_animal_index( index ), do: index+1
 
   def handle_cast( {:start}, state) do
     IO.puts "Empezando el contador"
